4c042c282e8424cb83298e69c03a74f4736415e3
getinthere <getinthere@naver.com>
2026-01-17 11:42:21 +0900

    init project

---
+++ src/main/java/dev/dote/qtrack/QtrackApplication.java
@@ -0,0 +1,13 @@
+package dev.dote.qtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dote/qtrack/QtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dote.qtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
